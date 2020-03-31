a362f9e5775471123dbe23e12415525f6a1e96d5
Lahindu Weerarathna <Lahindu_105097@dialog.dialoggsm.com>
2020-04-01 01:13:03 +0530

    deploy.sh changes

--- Jenkins/Deploy.sh
+++ Jenkins/Deploy.sh
@@ -2,10 +2,10 @@
 
 deploy_services(){
 
-	. ${WORKSPACE}/Jenkins/${ENVIRONMENT}/env
+	#. ${WORKSPACE}/Jenkins/${ENVIRONMENT}/env
 
     
-    echo "docker rm -f qms_backend"
+    echo "docker rm -f 	"
     docker rm -f qms_backend
 	
 	echo "docker run --restart always --name qms_backend -d -p 8085:8080 qms_backend:v0.${BUILD_NUMBER}"
