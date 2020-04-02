08905c64fb8ae9ef5753053a08dff4a55321a6fd
Sandaruwan Wijerathne <sandaruwan.15@itfac.mrt.ac.lk>
2020-04-02 22:56:46 +0530

    Create the SecretDto.java

---
+++ src/main/java/lk/uom/fit/qms/dto/SecretDto.java
@@ -0,0 +1,13 @@
+package lk.uom.fit.qms.dto;
+
+public class SecretDto {
+    public String secret;
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+}
